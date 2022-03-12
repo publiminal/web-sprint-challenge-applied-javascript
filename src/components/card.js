a455e7c14dc9c9ba6ec6ce54e2962287a95bf700
Andres Bernal <publiminal@gmail.com>
2022-03-12 02:44:54 -0500

    added filters on click tabs

--- src/components/card.js
+++ src/components/card.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const Card = ({ headline, authorPhoto, authorName }) => {
+const Card = ({ headline, authorPhoto, authorName, cat }) => {
   // TASK 5
   // ---------------------
   // Implement this function, which should return the markup you see below.
@@ -31,12 +31,15 @@ const Card = ({ headline, authorPhoto, authorName }) => {
   headLine.classList.add('headline')
   author.classList.add('author')
   imgContainer.classList.add('img-container')
+  // card.classList.add(cat)
+  card.setAttribute('data-cat', cat)
 
   card.appendChild(headLine)
   card.appendChild(author)
   author.appendChild(imgContainer)
   imgContainer.appendChild(img)
   author.appendChild(span)
+  // card.dataset.topicTitle = topicTitle
 
   headLine.textContent = headline
   img.src = authorPhoto
@@ -44,11 +47,12 @@ const Card = ({ headline, authorPhoto, authorName }) => {
 
 
   function onClickCard() {
-    console.log('headline' , headline)
+    console.log('headline', headline)
+    console.log('data cat', card.dataset.cat)
   }
 
 
-  card.addEventListener('click' , onClickCard)
+  card.addEventListener('click', onClickCard)
 
   // card.listClass.add('card')
   // console.log('card ', card)
@@ -70,17 +74,50 @@ const cardAppender = (selector) => {
   function displayArticleByTopic(topicTitle, articlesByTopic) {
     const parent = document.querySelector(selector)
     // console.log('displayArticleByTopic : articlesByTopic ', articlesByTopic)
-    const __topicTitle = topicTitle
+    // const __topicTitle = topicTitle
     // console.log(articlesByTopic.length)
     articlesByTopic.forEach(article => {
+      article.cat = topicTitle
       // console.log(`displayArticleByTopic ::  ${__topicTitle}:::::  `, article.headline )
       // console.log('displayArticleByTopic')
       // card = 
       // console.log('card', card ); 
+      // parent.innerText = ''
       parent.appendChild(Card(article))
 
     })
   }
+
+  function setCategoriesFilters(topicTitles) {
+    const tabs = document.querySelectorAll('.tabs-container .topics .tab ')
+    const cardsContainer = document.querySelector(selector)
+    const cards = cardsContainer.children
+
+    tabs.forEach(tab => {
+      console.log('tab', tab.innerHTML)
+      tab.addEventListener('click', ev => {
+        console.log(ev.target.innerHTML)
+        const topicFilter = ev.target.innerHTML
+        Array.from(cards).forEach(card => {
+          //  card.dataset.cat.includes(topicFilter)
+          //  console.log(card.dataset.cat.includes(topicFilter))
+          card.classList.add('card-transition')
+          const isFiltered = topicFilter.includes(card.dataset.cat)
+          // console.log('isFiltered', isFiltered)
+          const opacityAttr = isFiltered  ? 1 : 0
+          // card.setAttribute('opacity', opacityAttr)
+          card.style.opacity = opacityAttr
+          // console.log('filteredCards', filteredCards)
+          // const displayAttr = isFiltered ? 'flex' : 'none'
+          // card.style.display = displayAttr
+          const visAttr = isFiltered ? 'initial' : 'hidden'
+          card.style.visibility = visAttr
+          
+        })
+      })
+    })
+  }
+
 
   function onDataSuccess({ data, status }) {
     console.log('onSucessAxios', status)
@@ -88,13 +125,19 @@ const cardAppender = (selector) => {
     // console.log('dataArticles', data.articles  );
     // console.log('data' , data )
     const topicTitles = Object.keys(data.articles)
+    setCategoriesFilters(topicTitles)
     // console.log('topicTitles' , topicTitles )
     // console.log('topicTitles javascript' , data.articles[topicTitles[0]] )
     topicTitles.forEach(topicTitle => {
       const articlesByTopic = data.articles[topicTitle]
+      articlesByTopic.cat = topicTitle
+      // console.log('articlesByTopic.cat', articlesByTopic.cat )
+      // console.log('articlesByTopic.headline', articlesByTopic.headline)
       // console.log(` --------- ArticlesByTopic ${topicTitle} ` , articlesByTopic)
       displayArticleByTopic(topicTitle, articlesByTopic)
     })
+
+    const datasetTest = document.querySelectorAll()
   }
 
   function onDataFail({ response/* ,  message */ }) {
@@ -115,3 +158,9 @@ const cardAppender = (selector) => {
 }
 
 export { Card, cardAppender }
+
+
+
+
+
+
